23c3674c75463ef55633e1e989f6def8bdd10896
Boguslaw Faja <boguslaw.faja@allegrogroup.com>
2017-11-08 17:58:26 +0100

    [wip] CacheBy query params

--- src/main/java/net/zymen/retryablerequests/RequestToChecksumService.java
+++ src/main/java/net/zymen/retryablerequests/RequestToChecksumService.java
@@ -1,25 +1,13 @@
 package net.zymen.retryablerequests;
 
-import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-
-import static org.slf4j.LoggerFactory.getLogger;
 
 @Service
 class RequestToChecksumService {
 
-    private static final Logger log = getLogger(RequestToChecksumService.class);
-
-    String requestToChecksum(HttpServletRequest request) {
-//        try {
-//            log.info("Parts: {}", request.getParts());
-//        } catch (IOException | ServletException e) {
-//            e.printStackTrace();
-//        }
+    String requestToChecksum(final HttpServletRequest request) {
 
         return request.getRequestURI();
     }
